e359983cbee427f0a8bdf965bdb3c2b913d402af
abhinay9676 <abhinay.kotagiri8@gmail.com>
2025-02-06 16:28:15 +0530

    updated password

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://devTinder:Sai1234@cluster0.asqjo.mongodb.net/devTinder");
+    await mongoose.connect("mongodb+srv://devTinder:PCbuyJ50P41zs5Qz@cluster0.asqjo.mongodb.net/devTinder");
     
     
 };
